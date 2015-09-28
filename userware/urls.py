c9c12be77e839ef0c1bee5ad4a2b2fba697bc67d
Val Neekman <val@neekware.com>
2015-09-27 20:27:46 -0400

    remove trailing slashes

--- userware/urls.py
+++ userware/urls.py
@@ -11,38 +11,38 @@ from .views import *
 urlpatterns = patterns('',
 
     url(
-        r'^login/$',
+        r'^login$',
         UserLoginView.as_view(),
         name='user_login'
     ),
 
     url(
-        r'^logout/$',
+        r'^logout$',
         UserLogoutView.as_view(),
         name='user_logout'
     ),
 
     url(
-        r'^password/change/$',
+        r'^password/change$',
         UserChangePassword.as_view(),
         name='user_password_change'
     ),
 
     url(
-        r'^delete/$',
+        r'^delete$',
         UserDeleteView.as_view(),
         name='user_delete_account'
     ),
 
     url(
-        r'^switch/on/$',
+        r'^switch/on$',
         UserSwitchOnView.as_view(),
         name='user_switch_on'
     ),
 
     # user forgot his/her password again. ask for username or email and send a reset link
     url(
-        r'^password/reset/request/$',
+        r'^password/reset/request$',
         auth_views.password_reset,
         {
             'password_reset_form': UserPasswordResetForm,
@@ -56,7 +56,7 @@ urlpatterns = patterns('',
 
     # an email has been sent to the provided email address with the link to reset password
     url(
-        r'^password/reset/request/sent/$',
+        r'^password/reset/request/sent$',
         auth_views.password_reset_done,
         {
             'template_name': util.get_template_path('password_reset_request_sent.html'),
@@ -66,7 +66,7 @@ urlpatterns = patterns('',
 
     # password reset link has been clicked on, forms allows for a new password and confirmation
     url(
-        r'^password/reset/set/new/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        r'^password/reset/set/new/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)$',
         auth_views.password_reset_confirm,
         {
             'set_password_form': UserSetPasswordForm,
@@ -78,7 +78,7 @@ urlpatterns = patterns('',
 
     # system has changed the password and redirect to this template for the final success message
     url(
-        r'^password/reset/complete/$',
+        r'^password/reset/complete$',
         auth_views.password_reset_complete,
         {
             'template_name': util.get_template_path('password_reset_is_complete.html'),
